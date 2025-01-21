6b88e657d22047deb78a7ba9972dfdd643aaabc8
gyuseon <mn5592@naver.com>
2025-01-21 17:18:03 +0900

    Section6-4.Least Recently Used

---
+++ Section06/Least_Recently_Used.java
@@ -0,0 +1,45 @@
+package 인프런.Section06;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Least_Recently_Used {
+    public static Deque<Integer> solution(int s, int[] arr) {
+
+        Deque<Integer> deque = new LinkedList<>();
+
+        for(int i : arr) {
+            if(deque.contains(i)) {
+                deque.remove(i);
+                deque.addFirst(i);
+            } else {
+                if(deque.size() == s) {
+                    deque.removeLast();
+                }
+                deque.addFirst(i);
+            }
+        }
+
+        return deque;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int s = scanner.nextInt();
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        Deque<Integer> result = solution(s, arr);
+        for(int i : result) {
+            System.out.print(i + " ");
+        }
+    }
+}
