a36c1d91adb89ad0aa96b665ec26a337169c3c57
gyuseon <mn5592@naver.com>
2025-01-21 16:38:44 +0900

    Section6-3. 삽입 정렬

--- Section06/삽입_정렬.java
+++ Section06/삽입_정렬.java
@@ -8,13 +8,12 @@ public class 삽입_정렬 {
         for(int i = 1; i < n; i++){
             int tmp = arr[i], j; //삽입 값을 tmp에 저장
             for(j = i-1; j >= 0; j--) {
-                if(arr[j] > tmp) // 비교 값이 tmp 보다 크다?
+                if(arr[j] > tmp) // 비교 값이 삽입 값 보다 크다?
                     arr[j+1] = arr[j]; // 한칸씩 뒤로 민다
                 else // 비교 값이 삽입 값보다 작으면 삽입할 곳을 찾았으므로 멈춘다.
                     break;
             }
             arr[j+1] = tmp;
-
         }
 
         return arr;
