d18896514fb490be3b03cbc4dd8230f74a497522
gyuseon <mn5592@naver.com>
2025-01-23 11:35:59 +0900

    Section6-7. 좌표 정렬

---
+++ Section06/좌표_정렬.java
@@ -0,0 +1,67 @@
+package 인프런.Section06;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class 좌표_정렬 {
+    static class Position {
+        int x;
+        int y;
+        public Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int compareTo(Position p) {
+            if(this.x > p.x)
+                return  1;
+            else if(this.x == p.x){
+                if(this.y > p.y)
+                    return 1;
+                else
+                    return -1;
+            } else
+                return -1;
+        }
+    }
+    public static Position[] solution(int n, Position[] arr) {
+
+        Arrays.sort(arr, new Comparator<Position>() {
+            @Override
+            public int compare(Position o1, Position o2) {
+                if(o1.x > o2.x)
+                    return 1;
+                else if(o1.x == o2.x) {
+                    if(o1.y > o2.y)
+                        return 1;
+                    else
+                        return -1;
+                } else
+                    return -1;
+            }
+        });
+
+        return arr;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        Position[] arr = new Position[n];
+
+        for(int i = 0; i < n; i++) {
+            Position p = new Position(scanner.nextInt(), scanner.nextInt());
+            arr[i] = p;
+        }
+
+        Position[] result = solution(n, arr);
+
+        for(Position p : result) {
+            System.out.println(p.x + " " + p.y);
+        }
+    }
+}
