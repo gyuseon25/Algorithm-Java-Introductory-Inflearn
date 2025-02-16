c2424d104cf6b7c24419a1a80ba7d62a93d6c590
gyuseon <mn5592@naver.com>
2025-02-16 22:53:58 +0900

    Section10-3. 최대 부분 증가수열(LIS)

---
+++ Section10/최대_부분_증가수열.java
@@ -0,0 +1,38 @@
+package 인프런.Section10;
+
+import java.util.*;
+
+public class 최대_부분_증가수열 {
+
+    static int[] dp;
+
+    public int solution(int[] arr) {
+        int answer = 0;
+        dp = new int[arr.length];
+        dp[0] = 1;
+        for(int i=1; i<arr.length; i++){
+            int max=0;
+            for(int j=i-1; j>=0; j--){
+                if(arr[j] < arr[i] && dp[j] > max) max = dp[j];
+            }
+            dp[i] = max + 1;
+            answer=Math.max(answer, dp[i]);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        최대_부분_증가수열 t = new 최대_부분_증가수열();
+
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.print(t.solution(arr));
+    }
+}
